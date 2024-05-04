fcf57c2bc3e4ce3812eab03535304ab4a9f795ed
blueblack319 <didwogns789@yonsei.ac.kr>
2024-05-04 09:23:59 +0900

    dp-1

---
+++ acmicpc/dp/2839.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int N;
+int dp[5001];
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> N;
+
+  dp[3] = 1;
+  dp[5] = 1;
+  for (int i = 6; i <= N; i++) {
+    if (dp[i - 3]) dp[i] = dp[i - 3] + 1;
+    if (dp[i - 5]) dp[i] = dp[i] ? min(dp[i], dp[i - 5] + 1) : dp[i - 5] + 1;
+  }
+  if (dp[N] == 0)
+    cout << -1;
+  else
+    cout << dp[N];
+}
