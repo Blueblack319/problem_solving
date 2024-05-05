de4e395efcc9debbbd4044da6aac3458b4604a7c
blueblack319 <didwogns789@yonsei.ac.kr>
2024-05-05 21:55:52 +0900

    dp

--- acmicpc/dp/1003.cpp
+++ acmicpc/dp/1003.cpp
@@ -1,9 +1,34 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
+/*
+dp[i] = dp[i - 1] + dp[i - 2]
+*/
+#define MAX_N 41
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+int dp[MAX_N][2];
+int T, n;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> T;
+
+  dp[0][0] = 1;
+  dp[0][1] = 0;
+
+  dp[1][0] = 0;
+  dp[1][1] = 1;
+
+  for (int i = 2; i < MAX_N; i++) {
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+    dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+  }
+
+  for (int i = 0; i < T; i++) {
+    cin >> n;
+    cout << dp[n][0] << " " << dp[n][1] << "\n";
+  }
 }
